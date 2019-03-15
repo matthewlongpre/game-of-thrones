9dfeccb6c2883c56041094103901f5a9f4619c86
Matt Longpre <mlongpre@itgroove.ca>
2019-03-15 08:44:00 -0700

    Init

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB2kZ6BcAcHJZ8cyM9FCCraJX9SBAlTCx0",
+    authDomain: "game-of-thrones-a2824.firebaseapp.com",
+    databaseURL: "https://game-of-thrones-a2824.firebaseio.com",
+    projectId: "game-of-thrones-a2824",
+    storageBucket: "game-of-thrones-a2824.appspot.com",
+    messagingSenderId: "859563812376"
+  };
+  firebase.initializeApp(config);
+
+  export default firebase;
