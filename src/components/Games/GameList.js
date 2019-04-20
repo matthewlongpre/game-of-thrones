3dbeb42656a90b3b3332dbd07329842f16e0de20
Matt Longpre <mlongpre@itgroove.ca>
2019-04-20 14:01:54 -0700

    Games buttons

--- src/components/Games/GameList.js
+++ src/components/Games/GameList.js
@@ -1,9 +1,14 @@
+import { Button } from "@material-ui/core";
 import React from "react";
 import { CardStyle } from "../Player/Card";
 import { PageHeadingRow } from "./../Scoreboard/Styles";
 
 export const GameList = ({ games, handleGameChange }) => {
-  const gameRows = games.map(game => <CardStyle key={game.id}><button onClick={() => handleGameChange(game.id)}>{game.displayName}</button></CardStyle>);
+  const gameRows = games.map(game =>
+    <CardStyle key={game.id}>
+      <Button variant="contained" color="primary" onClick={() => handleGameChange(game.id)}>{game.displayName}</Button>
+    </CardStyle>
+  );
   return (
     <div>
       <PageHeadingRow>
